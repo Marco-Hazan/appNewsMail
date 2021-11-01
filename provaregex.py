6b94af780c52a947aeb987b53141e61545a5ed4f
MarcoHazan <marco.hazan@studenti.unimi.it>
2021-11-01 19:09:10 +0100

    .

--- provaregex.py
+++
@@ -1,7 +0,0 @@
-import re
-string = "[ciao,bella]{11/08/2018}ok";
-pattern = re.compile("\[[A-Za-z0-9, ]*\]\{[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9][0-9][0-9]\}.+")
-if pattern.match(string):
-    print("true")
-else:
-    print("false")
